66528465af9d01e1228daf555642e0f4b95cb0e1
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-22 11:09:36 +0530

    Message

--- app.js
+++ app.js
@@ -70,5 +70,29 @@ document.addEventListener('DOMContentLoaded' , () => {
     }
     draw()
 
+    const undraw = () =>  {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+            squares[currentPosition + index].style.backgroundColor = ''
+
+        })
+    }
+
+    // undraw()
+
+    const control = (e) => {
+        if (e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keyup', control)
+
 
 })
