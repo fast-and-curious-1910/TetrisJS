e0db361ad5f341bb7b9bbb696f2ebb415fe7bead
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-22 10:30:48 +0530

    JS

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded' , () => {
-    
+    const grid = document.querySelector('.grid') // Get the main class "grid"
+    let squares = document.querySelectorAll('.grid div') // Get the div(s) inside the "grid" class. This will not effect other divs outside the "grid" class
+    var width = 10
+    const ScoreDis = document.querySelector('#score')
+    const startBtn = document.querySelector('#startBtn')
 })
